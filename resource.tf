0311dd70e9162bb7aa3599f090a0eed736d3ce35
rishbh2168 <rishabhrgupta2168@gmail.com>
2025-07-16 09:53:13 +0000

    change123

--- resource.tf
+++ resource.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "storeterra" {
 }
  
 resource "azurerm_storage_account" "saterra" {
-    name                     = "saterra"
+    name                     = "saterra9026891562"
     resource_group_name      = azurerm_resource_group.storeterra.name
     location                 = azurerm_resource_group.storeterra.location
     account_tier             = "Standard"
