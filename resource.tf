bf74e9b493e1090ea06c2f254414fa8221cdea32
rishbh2168 <rishabhrgupta2168@gmail.com>
2025-07-16 09:26:27 +0000

    changess

--- resource.tf
+++ resource.tf
@@ -2,3 +2,22 @@ resource "azurerm_resource_group" "terra" {
   name     = "terra"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "storeterra" {
+    name     = "storeterra"
+    location = "East US"
+}
+ 
+resource "azurerm_storage_account" "saterra" {
+    name                     = "saterra"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "sacontainer"
+    storage_account_name  = azurerm_storage_account.saterra.name
+    container_access_type = "private"
+}
